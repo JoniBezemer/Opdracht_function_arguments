30c523f60070747a73062ebd44326be57f851b69
JoniBezemer <77683711+JoniBezemer@users.noreply.github.com>
2021-04-07 13:47:41 +0200

    Add files via upload

---
+++ 10-function exercise/script.js
@@ -0,0 +1,10 @@
+let paintWall = function (colorA, colorB){// de volgorde maakt uit
+    console.log('The wall has been painted ' + colorA);
+    console.log('The north wall has been painted ' + colorB)
+};
+
+let colorA = 'grey';
+let colorB = 'orange';
+
+paintWall(colorA);
+paintWall(colorB);
